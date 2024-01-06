f25c50381624c874f48c1c1e9ad94777c78b5897
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2024-01-06 12:53:58 +0300

    Daily | add 300 Longest Increasing Subsequence

---
+++ python/300.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/longest-increasing-subsequence/
+
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+Subsequence
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+"""
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+assert Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+assert Solution().lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
+assert Solution().lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
+assert Solution().lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6]) == 6
